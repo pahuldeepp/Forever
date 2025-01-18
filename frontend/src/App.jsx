b1ceedfe47ae949efee903a48dbfaa93e6f39f63
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2025-01-18 18:45:27 +0530

    Home Page Complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import PlaceOrder from './pages/PlaceOrder';
 import Orders from './pages/Orders';
 import Collection from './pages/Collection'; 
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/orders" element={<Orders />} />
       </Routes>
+      <Footer/>
     </div>
   );
 };
