e04b6f203ffe27d0592a0a592be1b8271e67c710
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2025-02-02 00:06:46 +0530

    Add CSS

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import Login from './components/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token')?localStorage.getItem('token'):''); 
   useEffect(()=>{
