6c6804f27db0dade449f8ded9adca3500a6a291d
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2025-02-11 23:08:30 +0530

    Modify function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -37,7 +37,14 @@ const placeOrderRazorpay = async (req, res) => {
 
 // All orders data for admin panel
 const allOrders = async (req, res) => {
-
+    try{
+        const orders = await orderModel.find({})
+        res.json({success: true, orders})
+    }
+    catch(error){
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 // User order data for frontend
@@ -55,7 +62,15 @@ const userOrders = async (req, res) => {
 
 // Update order status
 const updateStatus = async (req, res) => {
-
+    try{
+        const {orderId, status} = req.body
+        await orderModel.findByIdAndUpdate(orderId, {status})
+        res.json({success: true, message: "Order Status Updated"})
+    }
+    catch (error){
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 export {placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, updateStatus, userOrders}
