debe2178ab9e01643f1d380eaa464dcc09c4a035
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2025-02-11 20:35:56 +0530

    Add Cart frontend

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -8,7 +8,8 @@ const Cart = () => {
   const { products, currency, cartItems, updateQuantity, navigate} = useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
   useEffect(()=>{
-    const tempData = [];
+    if(products.length > 0){
+      const tempData = [];
     for(const items in cartItems){
       for(const item in cartItems[items]){
         if(cartItems[items][item] > 0){
@@ -21,7 +22,9 @@ const Cart = () => {
       }
     }
     setCartData(tempData);
-  }, [cartItems])
+    }
+    
+  }, [cartItems, products])
   return (
     <div className='border-t pt-14'>
       <div className='text-2xl mb-3'>
