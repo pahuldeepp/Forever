f5ef20760f09e9a1fe06e2b18cadc54fc2949ed1
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2025-02-11 20:42:20 +0530

    Add method in order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,28 @@
-
+import orderModel from "../models/orderModel.js"
+import userModel from "../models/userModel"
 
 const placeOrder = async (req, res) => {
+    try {
+        const { userId, items, amount, address} = req.body
+        const orderData = {
+            userId,
+            items,
+            amount,
+            paymentMethod: "COD",
+            payment: false,
+            date: Date.now()
+        }
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
 
+        await userModel.findByIdAndUpdate(userId, {cartData: {}})
+
+        res.json({success: true, message: "Order Placed"})
+    }
+    catch (error){
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 // Placing orders using stripe method
