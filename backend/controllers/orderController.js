61af6ba7ccf658433a02e65cf1c293e7ee526b47
Divanshi Jain <121867251+DivanshiJain2005@users.noreply.github.com>
2025-02-11 20:36:26 +0530

    Add orders route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,32 @@
+
+
+const placeOrder = async (req, res) => {
+
+}
+
+// Placing orders using stripe method
+const placeOrderStripe = async (req, res) => {
+
+}
+
+// Placing orders using Razorpay method
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All orders data for admin panel
+const allOrders = async (req, res) => {
+
+}
+
+// User order data for frontend
+const userOrders = async (req, res) => {
+
+}
+
+// Update order status
+const updateStatus = async (req, res) => {
+
+}
+
+export {placeOrder, placeOrderRazorpay, placeOrderStripe, allOrders, updateStatus, userOrders}
